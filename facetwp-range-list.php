73f5d4a5905c0d45cb14128c6fc1e5c629263447
mgibbs189 <mgibbs189@gmail.com>
2018-05-30 09:17:34 -0400

    Tagged 0.3

--- facetwp-range-list.php
+++ facetwp-range-list.php
@@ -2,7 +2,7 @@
 /*
 Plugin Name: FacetWP - Range List
 Description: Range list facet type
-Version: 0.2
+Version: 0.3
 Author: FacetWP, LLC
 Author URI: https://facetwp.com/
 GitHub URI: facetwp/facetwp-range-list
